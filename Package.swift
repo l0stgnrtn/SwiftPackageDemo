5a35261956cb82f5e68fc5296dfe9ed3b0e5f791
Nikita Toivokainen <nikmosyakov@me.com>
2020-10-08 20:04:36 +0300

    update manifest

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "SwiftPackageDemo",
+        .binaryTarget(name: "Util",
                       url: "https://github.com/l0stgnrtn/SwiftPackageDemo/raw/main/Util.xcframework.zip",
                       checksum: "58ad8281d8a7e3af3520e148a90d2abfc4080e28b60184d53bf708155549aaca")
     ]
