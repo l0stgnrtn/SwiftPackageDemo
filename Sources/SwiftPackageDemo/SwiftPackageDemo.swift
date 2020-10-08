72eff9299c088647cb051e1766e028a3763822f4
Nikita Toivokainen <nikmosyakov@me.com>
2020-10-08 17:56:24 +0300

    add dependencies

--- Sources/SwiftPackageDemo/SwiftPackageDemo.swift
+++ Sources/SwiftPackageDemo/SwiftPackageDemo.swift
@@ -1,3 +1,9 @@
+import KeychainSwift
+
 public struct SwiftPackageDemo {
     public var SwiftPackageDemo_text = "Hello, World!"
+    
+    public static func foo() {
+        let _ = KeychainSwift()
+    }
 }
